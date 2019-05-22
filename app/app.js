7d9faae537345820854cbb43d984b2779730f464
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-22 14:11:06 +0100

    functionalised the code and make it reusable

--- app/app.js
+++ app/app.js
@@ -14,116 +14,78 @@ const jsonParser = bodyParser.json();
 const app = express();
 const port = 3000;
 
-app.route('/tasks')
-    .get((req, res) => {
-        client.connect((err) => {
-            if (!err) {
-                const db = client.db(dbname);
-                db.collection('tasks').find({}).toArray((err, result) => {
-                    let dataResponse = {};
-                    if (!err) {
-                        if (result.length > 0) {
-                            dataResponse = {
-                                statusCode: 200,
-                                message: 'data retrieved',
-                                data: result
-                            };
-                            res.json(dataResponse);
-                        } else {
-                            dataResponse = {
-                                statusCode: 204,
-                                message: 'no data present in the db',
-                                data: []
-                            };
-                            res.json(dataResponse);
-                        }
-                    } else {
-                        dataResponse = {
-                            error: {
-                                statusCode: 500,
-                                message: err
-                            }
-                        }
-                        res.json(dataResponse);
-                    }
-                })
-            }
-        })
+app.get('/tasks', (request, response) => {
+    client.connect((err) => {
+        if(!err) {
+            const db = client.db(dbname);
+            getTasks(db, {}, (result) => {
+                if(result.length > 0) {
+                    response.json({ success: true, message: 'All the tasks retrieved', data: result });
+                } else {
+                    response.json({ success: false, message: 'No data found', data: [] });
+                }
+            })
+        }
     })
-    .post(jsonParser, (req, res) => {
-        let data = req.body;
-        client.connect((err) => {
-            if (!err) {
-                const db = client.db(dbname);
-                db.collection('tasks').insertOne(data, (err, result) => {
-                    let responseObject = {};
-                    if (!err) {
-                        if(result.result.ok) {
-                            responseObject = {
-                                statusCode: 201,
-                                message: 'Task added to the database',
-                                data: []
-                            };
-                            res.json(responseObject);
-                        } else {
-                            responseObject = {
-                                statusCode: 400,
-                                message: 'Database error, task not added to the database',
-                                data: []
-                            };
-                            res.json(responseObject);
-                        }
-                    } else {
-                        responseObject = {
-                            error: {
-                                statusCode: 500,
-                                message: err
-                            }
-                        }
-                        res.json(responseObject);
-                    }
-                })
-            }
-        })
-    })
+});
 
-app.route('/tasks/:id')
-    .put(jsonParser, (req, res) => {
-        let id = req.params.id;
-        client.connect((err) => {
-            if (!err) {
-                const db = client.db(dbname);
-                db.collection('tasks').updateOne({"_id": objectId(id)}, {$set:{completed: 1}}, (err, result) => {
-                    let responseObject = {};
-                    if (!err) {
-                        if (result.result.ok) {
-                            responseObject = {
-                                statusCode: 204,
-                                message: 'Task successfully updated',
-                                data: []
-                            };
-                            res.json(responseObject);
-                        } else {
-                            responseObject = {
-                                statusCode: 400,
-                                message: 'Task not updated',
-                                data: []
-                            };
-                            res.json(responseObject);
-                        }
-                    }  else {
-                        responseObject = {
-                            error: {
-                                statusCode: 500,
-                                message: err
-                            }
-                        }
-                        res.json(responseObject);
+app.post('/tasks', jsonParser, (request, response) => {
+    const newTask = request.body;
+    client.connect((err) => {
+        if(!err) {
+            const db = client.db(dbname);
+            addNewTask(db, newTask, (result) => {
+                if(result.insertedCount) {
+                    response.json({ success: true, message: 'Task added to the database', data: [] });
+                } else {
+                    response.json({ success: false, message: 'Sorry, something went wrong', data: [] });
+                }
+            })
+        }
+    })
+});
+
+app.put('/tasks/:id', (request, response) => {
+    const id = request.params.id;
+    client.connect((err) => {
+        if(!err) {
+            const db = client.db(dbname);
+            if (id.length === 24) {
+                completeTask(db, objectId(id), (result) => {
+                    if(result.modifiedCount) {
+                        response.json({ success: true, message: 'Task completed!', data: [] });
+                    } else {
+                        response.json({ success: false, message: 'Sorry, something went wrong', data: [] });
                     }
                 })
+            } else {
+                response.json({ success: false, message: 'The id is not valid', data: [] });
             }
-        })
+        }
     })
+});
+
+
+function getTasks(db, condition, callback) {
+    const collection = db.collection('tasks');
+    collection.find(condition).toArray((err, result) => {
+        callback(result);
+    })
+}
+
+function  addNewTask(db, input, callback) {
+    const collection = db.collection('tasks');
+    collection.insertOne(input, (err, result) => {
+        callback(result);
+    })
+}
+
+function completeTask(db, id, callback) {
+    const collection = db.collection('tasks');
+    collection.updateOne({"_id": id}, {$set: {completed: 1}}, (err, result) => {
+        callback(result);
+    })
+}
 
 app.listen(port, () => {
     console.log('Connected to server');
